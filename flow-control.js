86afed945afd752f551fc3ebaa3e53c84fbd0910
Rohit Kale <ruk22@cam.ac.uk>
2020-04-23 23:03:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,8 +26,8 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   return conditionToTest (age>=13 && age<=19)
-    valueToBeReturnedIfTrue: "You are a teenager"
-    valueToBeReturnedIfFalse: "You are not a teenager"
+    valueToBeReturnedIfTrue : "You are a teenager"
+    valueToBeReturnedIfFalse : "You are not a teenager"
 }
 
 function switchAge(age) {
