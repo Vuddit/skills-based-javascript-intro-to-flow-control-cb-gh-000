39b66f081dde7b2e3ddbd612188c22b012d9441c
Rohit Kale <ruk22@cam.ac.uk>
2020-04-23 23:26:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,6 +32,6 @@ function switchAge(age) {
     switch (age) {
       case (age>=13 && age<=19): console.log('You are a teenager')
       break;
-      default: console.log('You have an age');
+      default: console.log('You have an age')
     }
 }
