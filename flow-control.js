4bf82aedabba2650e3f02ba29681cc294f3201f9
Rohit Kale <ruk22@cam.ac.uk>
2020-04-23 23:31:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,31 +29,9 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-    var age = "10"
     switch(age) {
-      case "12":
-        console.log('You are a teenager');
-        break;
-      case "13":
-        console.log('You are a teenager');
-        break;
-      case "14":
-        console.log('You are a teenager');
-        break;
       case "15":
         return 'You are a teenager'
-      case "16":
-        console.log('You are a teenager');
-        break;
-      case "17":
-        console.log('You are a teenager');
-        break;
-      case "18":
-        console.log('You are a teenager');
-        break;
-      case "19":
-        console.log('You are a teenager');
-        break;
       default: console.log('You have an age');
     }
 }
